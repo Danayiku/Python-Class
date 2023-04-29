6cebca7ab81c6d418eb3ee2078d9f1fe25f2b7c7
Danayiku <130875022+Danayiku@users.noreply.github.com>
2023-04-29 19:26:29 -0400

    Baybay & Ponpon
    Baybay and Ponpon is so cute

--- Class1.py
+++ Class1.py
@@ -3,5 +3,10 @@ i = 1
 name = 'Dana'
 print(name)
 i_string = '1'
-print(name + i_string)
-#mokeys
+Bunny = 'Baybay'
+print(name + i_string) 
+Hamster = "Ponpon"
+print(Hamster)
+print("Pon Pon is so cute")
+print(name == Hamster)
+print(Bunny + Hamster)
