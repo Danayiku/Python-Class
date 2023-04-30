175e251f58e08823377088815da44f3c16601d31
Danayiku <130875022+Danayiku@users.noreply.github.com>
2023-04-29 20:05:14 -0400

    Fruits
    Yummy Fruits

---
+++ Class2.py
@@ -0,0 +1,8 @@
+Fruit = "Apple"
+Fruits = ["Apple", "Banana", "Cherry", "Strawberry", "Yum Yum", "Bay Bay", "Pon Pon"]
+# [] Array
+print(Fruits[5])
+for fr in Fruits :
+     #print(fr)
+     print('a')
+print('b')
