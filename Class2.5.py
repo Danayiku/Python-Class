3febc43334bf93cc6a845ec09448c4fa24382281
Danayiku <130875022+Danayiku@users.noreply.github.com>
2023-05-07 20:07:51 -0400

    Monkey

--- Class2.5.py
+++ Class2.5.py
@@ -4,11 +4,11 @@ for x in range(5):
 for b in range(1000):
     if b == 100:break 
     print(b)
-    from turtle import *
-color('red', 'yellow')
-begin_fill()
-for x in range(1000000):
-    if x == 3: break
-    forward(200)
-end_fill()
-done()
+for x in range(10): # 0 - 9
+    if x == 1: print('monkey')
+    if x == 2: print('banana')
+    if x == 3: print('gorilla')
+    if x == 4: print('yum yum')
+    if x == 5: print('bay bay')
+    if x == 9: print('too too')
+    if x == 10: print('ponpon')
