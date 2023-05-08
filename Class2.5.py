7df58772b2ef94ce9d7559900e6de25079a26b44
Danayiku <130875022+Danayiku@users.noreply.github.com>
2023-05-07 20:06:37 -0400

    Fruits

---
+++ Class2.5.py
@@ -0,0 +1,14 @@
+for x in range(5):
+    if x == 3: break
+    print(x)
+for b in range(1000):
+    if b == 100:break 
+    print(b)
+    from turtle import *
+color('red', 'yellow')
+begin_fill()
+for x in range(1000000):
+    if x == 3: break
+    forward(200)
+end_fill()
+done()
--- Class2.py
+++ Class2.py
@@ -2,7 +2,20 @@ Fruit = "Apple"
 Fruits = ["Apple", "Banana", "Cherry", "Strawberry", "Yum Yum", "Bay Bay", "Pon Pon"]
 # [] Array
 print(Fruits[5])
-for fr in Fruits :
+#for fr in Fruits :
      #print(fr)
-     print('a')
+     #print('a')
+b =  Fruits
+print(b)
 print('b')
+print(range(3))
+for x in range(0,5):
+    print(x)
+for baybay in range(0,100):
+     print(baybay)
+for a in range(0,10):
+    print(Fruits[4])
+    x = 0
+for a in range(0,5):
+    x = x + a
+print(x)
