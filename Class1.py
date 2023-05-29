a9703ff7b2fcb1b7cb21e028dbcf3a8923f51508
Danayiku <130875022+Danayiku@users.noreply.github.com>
2023-05-29 19:55:16 -0400

    turtle

--- Class1.py
+++ Class1.py
@@ -1,10 +1,7 @@
 print('Hello World')
-i = 1
 name = 'Dana'
 print(name)
-i_string = '1'
 Bunny = 'Baybay'
-print(name + i_string) 
 Hamster = "Ponpon"
 print(Hamster)
 print("Pon Pon is so cute")
---
+++ Class4.py
@@ -0,0 +1,10 @@
+from turtle import *
+#function
+begin_fill()
+color ("pink")
+for i in range (10000000000000000000000000000000000000000000):
+    forward(100)
+    right(67)
+end_fill()
+done ()
+circle (200,360,400)
